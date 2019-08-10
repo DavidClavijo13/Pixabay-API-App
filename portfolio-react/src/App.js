a9946d2a2c5895ade938e1d4fd4e98628cf36865
davichodavid <davichodavid@yahoo.com>
2019-08-10 11:38:10 -0400

    Set up routes with home, about,contact

--- portfolio-react/src/App.js
+++ portfolio-react/src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <h1>We Here bois</h1>
-    </div>
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
