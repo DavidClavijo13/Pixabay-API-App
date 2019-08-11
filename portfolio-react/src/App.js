324d3b465584ebb4a5c3350b3c587ae3e9453450
davichodavid <davichodavid@yahoo.com>
2019-08-11 11:13:11 -0400

    changed UI library choice, will go with Semantic UI

--- portfolio-react/src/App.js
+++ portfolio-react/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Home from "./Components/Home";
 import About from "./Components/About";
@@ -9,11 +9,9 @@ function App() {
   return (
     <>
       <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-        </Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
       </Router>
     </>
   );
